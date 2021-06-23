80b8f5f3a8b67aa4e6b626fdd432e90f271ef213
proHong <tjdgus0454@gmail.com>
2021-06-23 09:30:20 +0900

    [홍성현][프로젝트 생성]

---
+++ src/main/java/finger/edu/habicastle/HabicastleApplication.java
@@ -0,0 +1,13 @@
+package finger.edu.habicastle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabicastleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabicastleApplication.class, args);
+    }
+
+}
